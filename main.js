22a097c43db6e84c1a99dfa3d07c4e372fa06d67
Gabriel <gabriel.slmendoza@gmail.com>
2022-10-30 21:07:01 -0500

    update implementation

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 import { Cropper } from "./lib/main";
 
 const cropper = new Cropper({
+	container: '#real-cropper',
 	crop: '.edit',
 	reset: '.delete',
 	preview: '.preview',
-	mimeType: 'image/png',
+	mimeType: '.png,.jpg,.jpeg',
 	onInvalidType: () => alert('the selected image type is invalid')
-})
-
-cropper.render('#real-cropper')
+});
